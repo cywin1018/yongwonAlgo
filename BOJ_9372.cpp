cf743ae88ccf798d39a8e03ba00c0a3b3dd5f852
cywin1018 <cywin0821@gmail.com>
2023-12-26 10:50:28 +0900

    commit and sloved 12/25

---
+++ BOJ_9372.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#pragma GCC optimize("O3")
+#define ll long long
+#define ull unsigned long long
+#define  pii pair<int,int>
+#define MOD 1000000000
+const ll INF = 10e12+10;
+using namespace std;
+
+
+int main() {
+    ios_base::sync_with_stdio(false), cin.tie(0);
+    int t; cin >> t;
+    while(t--){
+        int n,m; cin >> n >> m;
+        for(int i=0;i<m;i++){
+            int a,b; cin >> a >> b;
+        }
+        cout << n-1 << '\n';
+    }
+
+}
